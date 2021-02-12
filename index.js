fa97f04e4f094d4805c7f9cceae4734a4237d741
SethMacP <macpherson.seth@gmail.com>
2021-02-12 14:16:31 -0800

    index.js setup to run on port 5000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require('./server');
+
+const port = 5000
+
+server.listen(port, ()=>{
+    console.log(`Server is running @ http://localhost:${port}`)
+})
